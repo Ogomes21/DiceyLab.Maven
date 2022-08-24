207d985ebe736b3c107c4dc23094b88a6a55890b
octavia <Ogomes21@gmail.com>
2022-08-23 23:07:16 -0400

    Finished all tests.

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,8 +1,38 @@
 
 public class Bins {
-    //Create a tracking class Bins that can be used to track these results.
+    // Bins should have an array or list of some sort.
+    // That is meant to track the # of times all possible dice roll sums have been reached
+    // If there is a die object meant to represent two dice, the corresponding
+    // Bins class should be able to track every sum between 2 and 12
+    // If it were for 3, it would be between 3 - 18. The lowest roll combo and the highest combo
+    // Arrays start at 0 so adjust the 10, it'll be accurate
+    // 0 will always be equal to the lowest possible roll. Subtract the number of dice to get your min.
 
-    Bins results = new Bins(2, 12); // for bins from 2..12
-    Integer numberOfTens = results.getBin(10); // returns the number of tens in the 10 bin
-    results.incrementBin(10); // should increment bin # 10
+    private static int min; // minimum roll value
+    private static int max; // maximum roll value
+    static int[] bin;
+
+    public Bins() {
+        this.min = 1;
+        this.max = 6;
+        this.bin = new int[max + 1];
+    }
+
+    public Bins(int min, int max) {
+        // Look into accessed via instance reference
+        this.min = min;
+        this.max = max;
+        this.bin = new int[max + 1];
+    }
+
+    public void incrementBin(int num) {
+        bin[num - min]++;
+    }
+
+    // Create a tracking class Bins that can be used to track these results.
+
+    public Integer getBin(int tossSumToGetCountOf) {
+        return bin[tossSumToGetCountOf - min];
+    }
 }
+
--- src/test/java/BinsTest.java
+++ src/test/java/BinsTest.java
@@ -1,2 +1,39 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BinsTest {
+
+    @Test
+    public void incrementBinTest() {
+
+        // Given
+        Bins bin = new Bins(); // Created a Bin object
+
+        // When
+        bin.incrementBin(5);
+
+        // Then
+        int actual = bin.getBin(5);
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBin() {
+
+        //Given
+        Bins bin = new Bins();
+        bin.incrementBin(1);
+        bin.incrementBin(2);
+        bin.incrementBin(2);
+        //Checking for the sum of 1. It came back once. Actual checks for 1. 1/3
+
+        //When
+        int actual = bin.getBin(1);
+        int expected = 1;
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
