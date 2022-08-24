4967a4423ced84aad3086f9a1e97acd94ae03831
octavia <Ogomes21@gmail.com>
2022-08-23 23:33:10 -0400

    Completed lab.

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,55 @@
 public class Simulation {
+    public static void main(String[] args) {
+        entryMessage();
+        Simulation simulation = new Simulation(2, 1000000);
+        simulation.run(2,1000000);
+        simulation.printResults();
+    }
 
+    private Dice dice;
+    private Bins bins;
+    private int min;
+    private int max;
+    private Integer numOfDice;
+    private Integer numOfToss;
 
+    public Simulation (Integer numOfDice, Integer numOfToss) {
+        min = numOfDice;
+        max = numOfDice * 6;
+        this.numOfDice = numOfDice;
+        this.numOfToss = numOfToss;
+        this.bins = new Bins(min, max);
+        this.dice = new Dice(numOfDice);
+    }
 
+    public static void run(Integer numOfDice, Integer numOfToss) {
+        Dice dice = new Dice (numOfDice);
+        Bins bins = new Bins (numOfDice, numOfDice *6); //Fix out of bounds with parameter
+
+        for (int i = 0; i < numOfToss; i++) {
+            bins.incrementBin(dice.tossAndSum());
+        }
+    }
+
+    public void printResults() {
+        String asterisk = "";
+        int result = 0;
+        double percent = 0;
+
+    for (int i = 0; i <= (max-min); i++) {
+        result = bins.getBin(i + 2);
+        percent = ((double) result) / numOfToss;
+        for (int j = 0; j < (Math.round(percent * 100)); j++) {
+            asterisk += "*";
+        }
+        System.out.println(String.format("%4 : %10d: %.2f %s", min + i, result, percent, asterisk));
+        asterisk = "";
+    }
+
+    }
+
+    public static void entryMessage() {
+        System.out.println("***\nSimulation of 2 dice tossed for 100000 times.\n" +
+                "***");
+    }
 }
